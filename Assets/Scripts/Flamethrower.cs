c2707fc1174fcc9b3c4b205f6e7119d73a818288
kulkarnisheel@gmail.com <kulkarnisheel@gmail.com>
2022-11-23 17:14:12 -0800

    added health vfx

--- Assets/Scripts/Flamethrower.cs
+++ Assets/Scripts/Flamethrower.cs
@@ -6,22 +6,47 @@ using UnityEngine.VFX;
 public class Flamethrower : MonoBehaviour
 {
     public VisualEffect ft;
+    public VisualEffect health;
+    public VisualEffect thunder;
     // Start is called before the first frame update
     void Start()
     {
         ft.Stop();
+        health.Stop();
+        thunder.Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        // flamethrower
+        if (Input.GetKeyDown("1"))
         {
             ft.Play();
         }
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetKeyUp("1"))
         {
             ft.Stop();
+        }
+
+        // health
+        if (Input.GetKeyDown("2"))
+        {
+            health.Play();
+        }
+        if (Input.GetKeyUp("2"))
+        {
+            health.Stop();
+        }
+
+        // thunder
+        if (Input.GetKeyDown("3"))
+        {
+            thunder.Play();
+        }
+        if (Input.GetKeyUp("3"))
+        {
+            thunder.Stop();
         }
     }
 }
