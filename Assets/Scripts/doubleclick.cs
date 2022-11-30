9c13d700c7bdfebeaa9b03de22d4dbcf738d57c0
yokijyfr <103284162+yokijyfr@users.noreply.github.com>
2022-11-30 14:08:05 -0800

    update character info

--- Assets/Scripts/doubleclick.cs
+++ Assets/Scripts/doubleclick.cs
@@ -19,7 +19,7 @@ public class doubleclick : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonUp(0))
+        if(Input.GetMouseButtonUp(1))
         {
             leftClickNum += 1;
         }
